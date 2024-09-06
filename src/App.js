afe92e910ee6a25e9c26054e4b53dd78f46d674e
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-09-07 00:50:28 +0900

    ルーティング追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Toppage from "./pages/Toppage";
+import CreateRoom from "./pages/CreateRoom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Toppage />} />
+        <Route path="/createroom" element={<CreateRoom />} />
+      </Routes>
+    </Router>
   );
 }
 
