f8e7525ffa5e32a1e465e57e2fc5b3a65ab4bbc5
yamanetosiki <c1419395@st.kanazawa-it.ac.jp>
2024-09-10 00:20:33 +0900

    レイアウト修正

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -1,47 +1,44 @@
 import React, { useState } from 'react'
-// import "../App.css";
 import "./CreateRoom.css"
-import "../pages/Toppage.css"
 import Button from '../components/Button_orange/Button_orange';
 
 const CreateRoom = () => {
-  const[selectedValue,setSelectedValue] = useState('')
+  const [selectedValue, setSelectedValue] = useState('')
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
   };
 
   return (
-    <div>
+    <div className='CreateRoom'>
       <div className="titleName">タイトル</div>
-        
+
       <form className='inputform'>
         <input
           type='text'
           className="link-input_title"
         />
-      </form>
+        <div className='upload-image-num-select'>
+          <span className='nanigaiikana'>アップロード写真枚数</span>
 
-      <div className='nanigaiikana'>アップロード写真枚数</div>
-
-      <div className='selectbox'>
-        <select value={selectedValue} onChange={handleChange}>
-          <option value="">選択してください</option>
-          <option value="5枚">5枚</option>
-          <option value="6枚">6枚</option>
-          <option value="7枚">7枚</option>
-          <option value="8枚">8枚</option>
-
-        </select>
-      </div>
-      <div className="toppage">
-        <div className='attention'>※注意<br/>参加人数上限は4人です。<br/>写真アップロード上限は8枚です。</div>
-          <div className='create'>
-            <Button>
-              作成
-            </Button>
+          <div className='selectbox'>
+            <select value={selectedValue} onChange={handleChange}>
+              <option value="">選択してください</option>
+              <option value="5枚">5枚</option>
+              <option value="6枚">6枚</option>
+              <option value="7枚">7枚</option>
+              <option value="8枚">8枚</option>
+            </select>
+          </div>
         </div>
-      </div>
-    </div>
+        <div className='create'>
+          <Button type="submit">
+            作成
+          </Button>
+
+        </div >
+      </form>
+      <div className='attention'>※注意<br />参加人数上限は4人です。<br />写真アップロード上限は8枚です。</div>
+    </div >
   );
 };
 
