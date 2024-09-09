325e8b70a2c62cff4c27bbe294ddb8d50dff2813
yamanetosiki <c1419395@st.kanazawa-it.ac.jp>
2024-09-09 20:40:00 +0900

    部屋作成画面

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -1,10 +1,48 @@
-import React from 'react'
-import "../App.css";
+import React, { useState } from 'react'
+// import "../App.css";
+import "./CreateRoom.css"
+import "../pages/Toppage.css"
+import Button from '../components/Button_orange/Button_orange';
 
 const CreateRoom = () => {
-  return (
-    <div>CreateRoom</div>
-  )
-}
+  const[selectedValue,setSelectedValue] = useState('')
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
 
-export default CreateRoom
+  return (
+    <div>
+      <div className="titleName">タイトル</div>
+        
+      <form className='inputform'>
+        <input
+          type='text'
+          className="link-input_title"
+        />
+      </form>
+
+      <div className='nanigaiikana'>アップロード写真枚数</div>
+
+      <div className='selectbox'>
+        <select value={selectedValue} onChange={handleChange}>
+          <option value="">選択してください</option>
+          <option value="5枚">5枚</option>
+          <option value="6枚">6枚</option>
+          <option value="7枚">7枚</option>
+          <option value="8枚">8枚</option>
+
+        </select>
+      </div>
+      <div className="toppage">
+        <div className='attention'>※注意<br/>参加人数上限は4人です。<br/>写真アップロード上限は8枚です。</div>
+          <div className='create'>
+            <Button>
+              作成
+            </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateRoom;
