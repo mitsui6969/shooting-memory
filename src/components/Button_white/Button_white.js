1c29eeee17f7ad7ca0586647069895f2d2d0bd58
yamanetosiki <c1419395@st.kanazawa-it.ac.jp>
2024-09-09 20:38:42 +0900

    Buttonのコンポーネント分けました。

---
+++ src/components/Button_white/Button_white.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Button_white.css'; // 外部CSSファイルをインポート
+
+const Button = ({ children, ...rest }) => {
+    return (
+        <button className="button-white" {...rest}>
+            {children}
+        </button>
+    )
+}
+
+export default Button;
+
