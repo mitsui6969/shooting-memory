309ccfe1215218b36be14e949f5bf30854749325
ko-tarou <akokoa1221@gmail.com>
2024-09-09 15:35:43 +0900

    bodyの縦横修正

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Toppage />} />
-        <Route path="/create-room" element={<CreateRoom />} />
+        <Route path="/createroom" element={<CreateRoom />} />
         <Route path="/waitroom" element={<WaitRoom />} />
         <Route path="/shooting-screen" element={<ShootingScreen />} />
       </Routes>
