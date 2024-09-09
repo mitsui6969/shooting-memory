b50d59babfed7b581082a82162cf3e92a69d812b
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-09-09 18:45:44 +0900

    射的画面追加

--- src/pages/ShootingScreen.js
+++ src/pages/ShootingScreen.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
+import "../styles/ShootingScreen.css";
+import bearImage from "../assets/image/bear.png";
+import usagiImage from "../assets/image/usagi.png";
+import weddingbearImage from "../assets/image/wedding_bear.png";
+import presentImage from "../assets/image/present_box.png";
+import beargirlImage from "../assets/image/wedding_bear_female.png";
 
 const ShootingScreen = () => {
   useEffect(() => {
@@ -17,7 +23,18 @@ const ShootingScreen = () => {
     users();
   }, []);
 
-  return <div>ShootingScreen</div>;
+  return (
+    <div className="shooting-container">
+      <h2>一つ的を選んでください</h2>
+      <div className="target-container">
+        <img src={bearImage} alt="target" />
+        <img src={usagiImage} alt="target" />
+        <img src={weddingbearImage} alt="target" />
+        <img src={presentImage} alt="target" />
+        <img src={beargirlImage} alt="target" />
+      </div>
+    </div>
+  );
 };
 
 export default ShootingScreen;
