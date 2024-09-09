091cd5e3b393545c677386f23ee12873a8225e15
yamanetosiki <c1419395@st.kanazawa-it.ac.jp>
2024-09-10 01:18:19 +0900

    フレーム選択画面

--- src/pages/FrameSelection.js
+++ src/pages/FrameSelection.js
@@ -8,7 +8,7 @@ const FrameSelection = () => {
             <h2>フレームを選択してください</h2>
             <div className='frames'>
                 <div className='frame nomal'></div>
-                <div className='frame onborder'></div>
+                <div className='frame onborder'></div>               
                 <div className='frame notitle'></div>
             </div>
         </div>
