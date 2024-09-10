c1be5677dbe1f97b075facc0689425480f592624
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-09-10 19:39:18 +0900

    shooting-screen

--- src/pages/ShootingScreen.js
+++ src/pages/ShootingScreen.js
@@ -1,38 +1,47 @@
-import React, { useEffect } from "react";
-import { db } from "../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import React, { useState } from "react";
 import "../styles/ShootingScreen.css";
 import bearImage from "../assets/image/bear.png";
 import usagiImage from "../assets/image/usagi.png";
 import weddingbearImage from "../assets/image/wedding_bear.png";
 import presentImage from "../assets/image/present_box.png";
-import beargirlImage from "../assets/image/wedding_bear_female.png";
 
 const ShootingScreen = () => {
-  useEffect(() => {
-    const users = async () => {
-      try {
-        const response = await getDocs(collection(db, "rooms"));
-        response.forEach((doc) => {
-          console.log(doc.id, doc.data());
-        });
-      } catch (err) {
-        console.error(err.message);
-      }
-    };
-    users();
-  }, []);
+  const [showSquare, setShowSquare] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleClick = () => {
+    setShowSquare(true);
+    setIsClosing(false);
+  };
+
+  const handleNext = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setShowSquare(false);
+      setIsClosing(false);
+    }, 1000);
+  };
 
   return (
     <div className="shooting-container">
       <h2>一つ的を選んでください</h2>
       <div className="target-container">
-        <img src={bearImage} alt="target" />
-        <img src={usagiImage} alt="target" />
-        <img src={weddingbearImage} alt="target" />
-        <img src={presentImage} alt="target" />
-        <img src={beargirlImage} alt="target" />
+        <img src={bearImage} alt="target" onClick={handleClick} />
+        <img src={usagiImage} alt="target" onClick={handleClick} />
+        <img src={weddingbearImage} alt="target" onClick={handleClick} />
+        <img src={presentImage} alt="target" onClick={handleClick} />
+        <img src={presentImage} alt="target" onClick={handleClick} />
+        <img src={bearImage} alt="target" onClick={handleClick} />
+        <img src={usagiImage} alt="target" onClick={handleClick} />
+        <img src={bearImage} alt="target" onClick={handleClick} />
       </div>
+
+      {showSquare && (
+        <div className={`click-target ${isClosing ? "closing" : ""}`}>
+          <img src={bearImage} alt="sample" />
+          <button onClick={handleNext}>次の人へ</button>
+        </div>
+      )}
     </div>
   );
 };
