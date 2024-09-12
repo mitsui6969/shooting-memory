2a640f719974fb8df687a117718ce8ccf8b0ebbc
ko-tarou <akokoa1221@gmail.com>
2024-09-13 02:14:39 +0900

    一時保存

--- src/pages/FrameSelection.js
+++ src/pages/FrameSelection.js
@@ -1,34 +1,46 @@
+import React, { useState } from 'react';
 
-import React, { useNavigate } from 'react';
-
-import '../styles/FrameSelection.css'
-import titleBorder from '../assets/layoutSanple/title-border.png'
-import titleNone from '../assets/layoutSanple/title-none.png'
-import noneBorder from '../assets/layoutSanple/none-border.png'
-import noneNone from '../assets/layoutSanple/none-none.png'
+import '../styles/FrameSelection.css';
+import titleBorder from '../assets/layoutSanple/title-border.png';
+import titleNone from '../assets/layoutSanple/title-none.png';
+import noneBorder from '../assets/layoutSanple/none-border.png';
+import noneNone from '../assets/layoutSanple/none-none.png';
 
 const FrameSelection = () => {
-    const layoutCatalog = [titleBorder, titleNone, noneBorder, noneNone]
+  const layoutCatalog = [titleBorder, titleNone, noneBorder, noneNone];
+  const [selectedFrame, setSelectedFrame] = useState(null); // 選択されたフレームを保存
 
-    return (
-        <div className='frame-selection'>
-            <h2>フレームを選択してください</h2>
-            <div className='selection-area'>
-                <div className='frames'>
-                    {layoutCatalog.map((src, index) => (
-                        <div key={index} className='frame'>
-                            <img
-                                src={src}
-                                alt='layout'
-                                className='layout-sanple'
-                            />
-                        </div>
-                    ))}
-                </div>
-
+  return (
+    <div className='frame-selection'>
+      <h2>フレームを選択してください</h2>
+      
+      <div className='selection-area'>
+        <div className='frames'>
+          {layoutCatalog.map((src, index) => (
+            <div
+              key={index}
+              className={`frame ${selectedFrame === src ? 'selected' : ''}`} // 選択されたフレームにクラスを追加
+              onClick={() => setSelectedFrame(src)} // フレームをクリックしたときに選択
+            >
+              <img
+                src={src}
+                alt='layout'
+                className='layout-sample'
+              />
             </div>
+          ))}
         </div>
-    );
+	    </div>
+
+      {/* 選択されたフレームを画面中央に表示 */}
+      {selectedFrame && (
+        <div className='selected-frame-container'>
+          <h3>選択されたフレーム</h3>
+          <img src={selectedFrame} alt='Selected Layout' className='selected-frame' />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FrameSelection;
