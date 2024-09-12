9c496b5066a25c591837cfbcc29a16fbff7cc985
yamanetosiki <c1419395@st.kanazawa-it.ac.jp>
2024-09-12 21:51:26 +0900

    出揃い画面UI（完成）

--- src/pages/CompleteRoom.js
+++ src/pages/CompleteRoom.js
@@ -3,9 +3,13 @@ import { useSwipeable } from "react-swipeable";
 import "../styles/CompleteRoom.css";
 import "../App.css";
 import Button from '../components/Button_orange/Button_orange';
-import Image1 from "../assets/sample.png"
-import Image2 from "../assets/sample2.png"
-import Image3 from "../assets/sample3.png"
+import Image1 from "../assets/sample.png";
+import Image2 from "../assets/sample2.png";
+import Image3 from "../assets/sample3.png";
+
+// アップロードされた矢印画像
+import LeftArrowIcon from "../assets/image/leftarrow.png";
+import RightArrowIcon from "../assets/image/rightarrow.png";
 
 const imageList = [
   Image1,
@@ -20,7 +24,7 @@ const CompleteRoom = () => {
     onSwipedLeft: () => handleNext(),
     onSwipedRight: () => handlePrev(),
     preventDefaultTouchmoveEvent: true,
-    trackMouse: true
+    trackMouse: true,
   });
 
   const handleNext = () => {
@@ -33,25 +37,52 @@ const CompleteRoom = () => {
     );
   };
 
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
   return (
     <div>
       <div className="coordinate">
         <div>
           <h2>出揃いました</h2>
-          <div className="white-box" {...handlers}> {/* スワイプを有効にする */}
+          <div className="white-box" {...handlers}>
+            {/* 左矢印ボタン */}
+            <button className="left-arrow" onClick={handlePrev}>
+              <img src={LeftArrowIcon} alt="left arrow" />
+            </button>
+
+            {/* 画像表示 */}
             <img
               src={imageList[currentIndex]}
               alt="swipeable content"
               draggable="false"
-              onDragStart={(e) => e.preventDefault()} 
-              
+              onDragStart={(e) => e.preventDefault()}
               className="image nopointer"
             />
+
+            {/* 右矢印ボタン */}
+            <button className="right-arrow" onClick={handleNext}>
+              <img src={RightArrowIcon} alt="right arrow" />
+            </button>
           </div>
+
+          {/* インジケーター */}
+          <div className="indicators">
+            {imageList.map((_, index) => (
+              <span
+                key={index}
+                className={currentIndex === index ? "active" : ""}
+                onClick={() => goToSlide(index)}
+              >
+                ●
+              </span>
+            ))}
+          </div>
+
+          {/* 次へボタン */}
           <div className="image-button-container">
-            <Button onClick={handleNext}>
-              次へ
-            </Button>
+            <Button onClick={handleNext}>次へ</Button>
           </div>
         </div>
       </div>
