0e3b40bad613fd60399268b1a61dfd1f5fb42670
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-09-14 12:53:34 +0900

    ボタン追加＆edit-finに飛ぶ

--- src/pages/CollagePage.js
+++ src/pages/CollagePage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Frame from "../components/Frame/Frame";
 import "../styles/CollagePage.css";
 import testImage from "../assets/image/toppage-background.jpeg";
@@ -7,6 +8,7 @@ import testImage3 from "../assets/image/usagi.png"
 import { DndProvider, useDrag } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Button from "../components/Button_orange/Button_orange";
+import ButtonW from "../components/Button_white/Button_white";
 import html2canvas from "html2canvas";
 
 const DraggableImage = ({ src, index, removeImage, isDragged }) => {
@@ -41,18 +43,25 @@ const CollagePage = ({images, title="title", date="yyyy/mm/dd", selectColor=0, s
   const [imageSrc, setImageSrc] = useState(null);
   const [isModal, setIsModal] = useState(false);
 
+  const navigate = useNavigate();
+
   const removeImage = (index) => {
     setDraggedImages((prevDraggedImages) => [...prevDraggedImages, index])
   };
 
+  // 画像化
   const handleCompletion = () => {
     const target = document.getElementById('target-to-image')
     html2canvas(target, { scale:5, width:target.clientWidth, height:target.clientHeight, backgroundColor: null }).then((canvas) => {
-      // キャンバスをPNG形式のデータURLに変換
       const targetImgUri = canvas.toDataURL("image/png");
       setImageSrc(targetImgUri)
       setIsModal(true)
     });
+  }
+
+  // 出揃い画面にgo
+  const handletoEditFinPage = () => {
+    navigate('/edit-fin')
   }
 
   const handleModalClose = () => {
@@ -94,8 +103,19 @@ const CollagePage = ({images, title="title", date="yyyy/mm/dd", selectColor=0, s
 
     {isModal && (
       <div className="complet-modal">
-        <img src={imageSrc} className="collage-image"/>
-        <button onClick={handleModalClose}>close</button>
+        <div className="image-button-container">
+          <img src={imageSrc} className="collage-image"/>
+
+          <div className="button-container">
+            <div className="button-modal">
+              <ButtonW onClick={handleModalClose}>編集を続ける</ButtonW>
+            </div>
+
+            <div className="button-modal 4">
+              <Button onClick={handletoEditFinPage}>終了</Button>
+            </div>
+          </div>
+        </div>
       </div>
     )}
     </div>
